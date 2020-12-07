656943b1bd735aeb88c5a95e22f5896b9af95155
David Spigarelli <david@spigarelli.net>
2020-12-07 07:13:28 -0500

    Day 7: Baggage part 1

---
+++ puzzle7a.ex
@@ -0,0 +1,60 @@
+defmodule Baggage do
+    def parseRule(rule) do
+        sansBags = String.replace(rule, ~r/ bags?/, "", global: true)
+        [container, containee] = String.split(sansBags, " contain ")
+        containees = String.split(containee, ", ")
+        |> Enum.map(fn s -> String.replace(s, ~r/\.$/, "") end)
+        |> Enum.map(fn s -> String.replace(s, ~r/\d+ /, "", global: true) end)
+        |> Enum.filter(fn x -> x != "no other" end)
+        # IO.inspect([container, containees])
+        [container, containees]
+    end
+
+    def walkRules(rules, target) do
+        Stream.run(Stream.unfold(
+            { rules, [target] },
+            fn { rules, targets } -> 
+                # IO.inspect("Rules:")
+                # IO.inspect(rules)
+                # IO.inspect("Targets:")
+                # IO.inspect(targets)
+                { matches, newRules } = Enum.split_with(
+                    rules,
+                    fn [_, containees] -> Enum.any?(targets, fn x -> Enum.member?(containees, x) end) end
+                )
+                newTargets = Enum.uniq(Enum.map(matches, fn [first, _] -> first end) ++ targets)
+                # IO.inspect("Matches")
+                # IO.inspect(newTargets)
+                # IO.inspect("New Rules")
+                # IO.inspect(newRules)
+                IO.inspect("#{length(newTargets)}")
+
+                cond do
+                    length(matches) == 0 -> 
+                        nil
+                    true -> 
+                        { newRules, { newRules, newTargets }}
+                end                
+            end
+        ))
+    end
+end
+
+# s
+# IO.read(:stdio, :all)
+File.read!("input")
+|> String.split("\n")
+|> Enum.map(&String.trim/1)
+|> Enum.map(&Baggage.parseRule/1)
+|> Baggage.walkRules("shiny gold")
+# |> Enum.map(&Seating.toBinary/1)
+# |> Enum.sort()
+# |> Enum.reduce(0, fn x, acc -> 
+#     cond do 
+#         acc == 0 -> x
+#         x == (acc+1) -> x
+#         :true -> acc
+#     end
+# end)
+# |> (fn s -> IO.inspect(s+1) end).()
+# |> IO.inspect
