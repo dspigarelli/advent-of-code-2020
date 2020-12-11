f79c69184e68e13dbbb944bb186550b12936b293
David Spigarelli <david.spigarelli@expel.io>
2020-12-11 07:29:24 -0500

    Day 10: Adapter array

--- lib/day_10_adapter_array.ex
+++ lib/day_10_adapter_array.ex
@@ -3,14 +3,29 @@ defmodule AdventOfCode2020.Day10AdapterArray do
 
     def part_1(input) do
         input
-        |> parse()
-        |> (fn ls -> find_count_of_delta(ls, 3) * find_count_of_delta(ls, 1) end).()
+        |> parse
+        |> (fn ls -> [0] ++ ls ++ [List.last(ls)+3] end).()
+        |> (fn ls ->
+            Stream.unfold(
+                { 0, Enum.slice(ls, 0, 2) },
+                fn
+                    { _, [ _ ]} -> nil
+                    { index, pair } ->
+                        { { index, pair }, { index + 1, Enum.slice(ls, index + 1, 2) } }
+                end)
+            |> Enum.map(fn {_, [first, second] } -> second - first end)
+            end).()
+        |> Enum.reduce({ 0, 0 }, fn
+            1, { one, three } -> { one + 1, three }
+            3, { one, three } -> { one, three + 1 }
+            _, acc -> acc
+        end)
+        |> (fn { one, three} -> one * three end).()
     end
 
-    def part_2(input) do
-        # input
-        1
-    end
+    # def part_2(input) do
+    #    # input
+    #end
 
     defp parse(input) do
         input
@@ -18,8 +33,4 @@ defmodule AdventOfCode2020.Day10AdapterArray do
         |> Enum.map(&String.to_integer/1)
         |> Enum.sort
     end
-
-    defp find_count_of_delta(list, target) do
-        2
-    end
-end	
+end
--- test/day_10_adapter_array_test.exs
+++ test/day_10_adapter_array_test.exs
@@ -3,7 +3,7 @@ defmodule AdventOfCode2020.Day10AdapterArrayTest do
 
     import AdventOfCode2020.Day10AdapterArray
 
-    @example_input """
+    @example_input_1 """
     16
     10
     15
@@ -17,9 +17,61 @@ defmodule AdventOfCode2020.Day10AdapterArrayTest do
     4
     """
 
+    @example_input_2 """
+    28
+    33
+    18
+    42
+    31
+    14
+    46
+    20
+    48
+    47
+    24
+    23
+    49
+    45
+    19
+    38
+    39
+    11
+    1
+    32
+    25
+    35
+    8
+    17
+    7
+    9
+    4
+    2
+    34
+    10
+    3
+    """
+
     describe "part_1/1" do
-        test "multiple count of 1 jolt and 3 jolt" do
-            assert 35 = @example_input |> String.trim() |> part_1()
+        test "example 1" do
+            assert 35 = @example_input_1 |> String.trim() |> part_1()
         end
+
+        test "example 2" do
+            assert 220 = @example_input_2 |> String.trim() |> part_1()
+        end
+
+        test "with puzzle input" do
+            assert 1_656 = puzzle_input() |> String.trim() |> part_1()
+        end
+    end
+
+    describe "part_2/1" do
+        # test "" do
+        #    assert "" = @example_input |> String.trim() |> part_2()
+        # end
+
+        # test "with puzzle input" do
+        #    assert "" = puzzle_input() |> String.trim() |> part_2()
+        # end
     end
 end
