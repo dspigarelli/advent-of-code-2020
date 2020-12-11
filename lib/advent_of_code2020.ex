252a3932e7a959693a028e03ce7dc411fe6e298f
David Spigarelli <david.spigarelli@expel.io>
2020-12-11 07:28:52 -0500

    Add mix for starting a day

--- lib/advent_of_code2020.ex
+++ lib/advent_of_code2020.ex
@@ -6,6 +6,39 @@ defmodule AdventOfCode2020 do
   defmacro __using__(options) do
     quote do
       import AdventOfCode2020
+
+      @doc """
+      Inspired by https://github.com/scmx/advent-of-code-2020-elixir
+      """
+      def puzzle_input do
+        read_puzzle_input_for(__MODULE__, unquote(options))
+      end
     end
   end
+
+  @doc false
+  def read_puzzle_input_for(module, options) when is_atom(module) do
+    module
+    |> input_filename
+    |> read_puzzle_input_for(options)
+  end
+
+  def read_puzzle_input_for(filename, options) when is_binary(filename) do
+    case File.read(Path.join(["input", filename <> ".txt"])) do
+      {:ok, data} -> trim(data, Keyword.get(options, :trim))
+      {:error, _} -> nil
+    end
+  end
+
+  defp input_filename(module) do
+    module
+    |> to_string()
+    |> String.split(".")
+    |> Enum.at(2)
+    |> Macro.underscore()
+    |> String.replace(~r/(\w)(\d)/, "\\1_\\2")
+  end
+
+  defp trim(text, false), do: String.trim_trailing(text, "\n")
+  defp trim(text, _), do: String.trim(text)
 end
---
+++ lib/mix/tasks/day/new.ex
@@ -0,0 +1,80 @@
+defmodule Mix.Tasks.Day.New do
+    @moduledoc """
+    Generate a new day. Inspired by https://github.com/scmx/advent-of-code-2020-elixir
+    """
+
+    def run([name]) do
+        module_name = Macro.camelize(name)
+
+        create_input_file(name)
+        create_lib_file(name, module_name)
+        create_spec_file(name, module_name)
+    end
+
+    defp create_input_file(name) do
+        File.write("input/#{name}.txt", IO.read(:stdio, :all))
+    end
+
+    defp create_lib_file(name, module_name) do
+        contents = generate_lib_file(name, module_name)
+        File.write("lib/#{name}.ex", contents)
+    end
+
+    defp create_spec_file(name, module_name) do
+        contents = generate_spec_file(name, module_name)
+        File.write("test/#{name}_test.exs", contents)
+    end
+
+    defp generate_lib_file(_name, module_name) do
+        """
+        defmodule AdventOfCode2020.#{module_name} do
+            use AdventOfCode2020
+
+            def part_1(input) do
+                input
+            end
+
+            # def part_2(input) do
+            #    # input
+            #end
+
+            defp parse(input) do
+                input
+            end
+        end
+        """
+    end
+
+    defp generate_spec_file(_name, module_name) do
+        """
+        defmodule AdventOfCode2020.#{module_name}Test do
+            use ExUnit.Case
+
+            import AdventOfCode2020.#{module_name}
+
+            @example_input \"\"\"
+            \"\"\"
+
+            describe "part_1/1" do
+                test "" do
+                    assert "" = @example_input |> String.trim() |> part_1()
+                end
+
+                test "with puzzle input" do
+                    assert "" = puzzle_input() |> String.trim() |> part_1()
+                end
+            end
+
+            describe "part_2/1" do
+                # test "" do
+                #    assert "" = @example_input |> String.trim() |> part_2()
+                # end
+
+                # test "with puzzle input" do
+                #    assert "" = puzzle_input() |> String.trim() |> part_2()
+                # end
+            end
+        end
+        """
+    end
+end
